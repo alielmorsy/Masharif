b4c2208fbc2cef8b7db75e4a834dba301ca5c2b0
Ali Elmorsy <ali.elmorsy19@gmail.com>
2026-02-18 18:43:51 +0200

    Some minimal cleaning

--- masharifcore/structure/Style.h
+++ masharifcore/structure/Style.h
@@ -18,71 +18,68 @@ namespace masharif {
     };
 
 
+    class Style {
+    public:
+        bool dirty = true;
 
-class Style {
-public:
-    bool dirty = true;
+        template<typename T, std::enable_if_t<
+            std::is_same_v<T, Dimensions> ||
+            std::is_same_v<T, CSSFlex> ||
+            std::is_same_v<T, MarginEdge> ||
+            std::is_same_v<T, PaddingEdge> ||
+            std::is_same_v<T, Edge> ||
+            std::is_same_v<T, BorderProperties>, int> = 0>
+        T &modify() {
+            dirty = true;
+            return getProperty<T>();
+        }
 
-    template<typename T, std::enable_if_t<
-        std::is_same_v<T, Dimensions> ||
-        std::is_same_v<T, CSSFlex> ||
-        std::is_same_v<T, MarginEdge> ||
-        std::is_same_v<T, PaddingEdge> ||
-        std::is_same_v<T, Edge> ||
-        std::is_same_v<T, BorderProperties>, int> = 0>
-    T& modify() {
-        dirty = true;
-        return getProperty<T>();
-    }
+        [[nodiscard]] const CSSFlex &flex() const { return m_flexProps; }
+        [[nodiscard]] const MarginEdge &margin() const { return m_marginProps; }
+        [[nodiscard]] const PaddingEdge &padding() const { return m_paddingProps; }
+        [[nodiscard]] const BorderProperties &border() const { return m_borderProps; }
+        [[nodiscard]] const Dimensions &dimensions() const { return m_dimensions; }
+        [[nodiscard]] const PositionOffsets &offsets() const { return m_offsets; }
 
-    [[nodiscard]] const CSSFlex& flex() const { return m_flexProps; }
-    [[nodiscard]] const MarginEdge& margin() const { return m_marginProps; }
-    [[nodiscard]] const PaddingEdge& padding() const { return m_paddingProps; }
-    [[nodiscard]] const BorderProperties& border() const { return m_borderProps; }
-    [[nodiscard]] const Dimensions& dimensions() const { return m_dimensions; }
-    [[nodiscard]] const PositionOffsets& offsets() const { return m_offsets; }
+    private:
+        template<typename T>
+        std::enable_if_t<std::is_same_v<T, CSSFlex>, T &> getProperty() {
+            return m_flexProps;
+        }
 
-private:
-    // Use function overloading instead of if constexpr
-    template<typename T>
-    std::enable_if_t<std::is_same_v<T, CSSFlex>, T&> getProperty() {
-        return m_flexProps;
-    }
+        template<typename T>
+        std::enable_if_t<std::is_same_v<T, MarginEdge>, T &> getProperty() {
+            return m_marginProps;
+        }
 
-    template<typename T>
-    std::enable_if_t<std::is_same_v<T, MarginEdge>, T&> getProperty() {
-        return m_marginProps;
-    }
+        template<typename T>
+        std::enable_if_t<std::is_same_v<T, PaddingEdge>, T &> getProperty() {
+            return m_paddingProps;
+        }
 
-    template<typename T>
-    std::enable_if_t<std::is_same_v<T, PaddingEdge>, T&> getProperty() {
-        return m_paddingProps;
-    }
+        template<typename T>
+        std::enable_if_t<std::is_same_v<T, BorderProperties>, T &> getProperty() {
+            return m_borderProps;
+        }
 
-    template<typename T>
-    std::enable_if_t<std::is_same_v<T, BorderProperties>, T&> getProperty() {
-        return m_borderProps;
-    }
+        template<typename T>
+        std::enable_if_t<std::is_same_v<T, Dimensions>, T &> getProperty() {
+            return m_dimensions;
+        }
 
-    template<typename T>
-    std::enable_if_t<std::is_same_v<T, Dimensions>, T&> getProperty() {
-        return m_dimensions;
-    }
+        template<typename T>
+        std::enable_if_t<std::is_same_v<T, PositionOffsets>, T &> getProperty() {
+            return m_offsets;
+        }
 
-    template<typename T>
-    std::enable_if_t<std::is_same_v<T, PositionOffsets>, T&> getProperty() {
-        return m_offsets;
-    }
-
-    // Property storage
-    CSSFlex m_flexProps;
-    MarginEdge m_marginProps;
-    PaddingEdge m_paddingProps;
-    PositionOffsets m_offsets;
-    BorderProperties m_borderProps;
-    Dimensions m_dimensions;
-};
-
+        // Property storage
+        CSSFlex m_flexProps;
+        MarginEdge m_marginProps;
+        PaddingEdge m_paddingProps;
+        PositionOffsets m_offsets;
+        BorderProperties m_borderProps;
+        Dimensions m_dimensions;
+    };
 };
 
 
